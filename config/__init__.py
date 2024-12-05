57747e32b04ad8d4e9413e044913a4c9a6676919
NaufalMYR <151520709+NaufalMYR@users.noreply.github.com>
2024-12-05 18:26:20 +0700

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -1,28 +1,16 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
-import os
-
-db = SQLAlchemy()
+from config.database import Config, firestore_client
 
 def create_app():
     app = Flask(__name__)
-
-    # Tambahkan konfigurasi CORS
     CORS(app)
-
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
-
-    db.init_app(app)
+    app.config.from_object(Config)
     JWTManager(app)
 
     with app.app_context():
         from routes import register_routes  # Gunakan impor relatif
         register_routes(app)
-
-        db.create_all()
 
     return app
