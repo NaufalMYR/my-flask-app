14ef1618a28865820e797fedea89ad2eb0efa193
NaufalMYR <151520709+NaufalMYR@users.noreply.github.com>
2024-12-05 18:26:51 +0700

    Update database.py

--- config/database.py
+++ config/database.py
@@ -1,9 +1,13 @@
 import os
 from dotenv import load_dotenv
+from google.cloud import firestore
 
 load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    PROJECT_ID = os.getenv('GCP_PROJECT_ID')
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    GOOGLE_APPLICATION_CREDENTIALS = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+
+# Inisialisasi Firestore
+firestore_client = firestore.Client()
